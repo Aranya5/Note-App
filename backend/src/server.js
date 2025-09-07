ad459bbcc16f6e1f6b908751a9a306031bcddccc
Aranya5 <aranyabasu5@gmail.com>
2025-09-07 17:09:05 +0530

    Database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import notesRouter from './routes/noteRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/notes", notesRouter);
 
+connectDB();
 
-
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+  console.log('Server is running on port:', PORT);
 });
