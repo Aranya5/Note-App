6e10c33b441979f313c4f2b18d3cf502145cc7ce
Aranya5 <aranyabasu5@gmail.com>
2025-10-03 19:16:40 +0530

    frontend packages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router'
+
+import CreatePage from './pages/CreatePage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import NoteDetailPage from './pages/NoteDetailPage.jsx'
+
+const App = () => {
+  return (
+    <div data-theme="forest">
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/create' element={<CreatePage />} />
+      <Route path='/note/:id' element={<NoteDetailPage />} />
+    </Routes>
+      
+    </div>
+  )
+}
+
+export default App
