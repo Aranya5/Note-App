72a8537f6b7b5dae4488c2c7f134bb0810790ef0
Aranya5 <aranyabasu5@gmail.com>
2025-10-03 17:22:07 +0530

    rate limiting

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,17 @@
+import ratelimit from "../config/upstash.js"
+
+const rateLimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("MyIP")
+    if (!success) {
+      return res.status(429).json({ message: 'Too many requests, please try again later.' })
+    }
+    next()
+  } catch (error) {
+    console.error('Rate limiting error:', error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+
+}
+
+export default rateLimiter;
