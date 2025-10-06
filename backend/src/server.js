5da108a81cf7153bd0a379c50580881dac2c5ef5
Aranya5 <aranyabasu5@gmail.com>
2025-10-06 22:16:09 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,36 +1,47 @@
-import express from 'express';
-import notesRouter from './routes/noteRoutes.js';
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
-import rateLimiter from './middleware/rateLimiter.js';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: "http://localhost:5173"
-})); // Enable CORS for all routes
-app.use(express.json()); // Middleware to parse JSON bodies
-app.use(rateLimiter); // Apply rate limiting middleware
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-
+// our simple custom middleware
 // app.use((req, res, next) => {
-//   console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
 //   next();
 // });
 
-app.use("/api/notes", notesRouter); 
+app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-  console.log('Server is running on port:', PORT);
+    console.log("Server started on PORT:", PORT);
+  });
 });
-}).catch((error) => {
-  console.error('Failed to connect to the database:', error);
-});
-
-
